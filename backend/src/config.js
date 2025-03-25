e5f6f72fb39899bbce390ff7b3136b0aaccb697f
Gabriel <gaboquintana10@gmail.com>
2025-03-25 15:22:03 -0600

    Realice cerrar sesion

---
+++ backend/src/config.js
@@ -0,0 +1,22 @@
+import dotenv from "dotenv";
+
+// Ejecutamos la libreria
+// para acceder al .env
+dotenv.config();
+
+export const config = {
+  db: {
+    URI: process.env.DB_URI,
+  },
+  server: {
+    port: process.env.PORT,
+  },
+  JWT: {
+    secret: process.env.JWT_SECRET,
+    expiresIn: process.env.JWT_EXPIRES,
+  },
+  emailAdmin: {
+    email: process.env.ADMIN_EMAIL,
+    password: process.env.ADMIN_PASSWORD,
+  },
+};
---
+++ backend/src/controllers/logoutController.js
@@ -0,0 +1,8 @@
+const logoutController={};
+ 
+logoutController.logout = (req, res) => {
+res.clearCookie("authToken");
+return res.json({ message: "se cerro sesion" });
+}
+ 
+export default logoutController;
